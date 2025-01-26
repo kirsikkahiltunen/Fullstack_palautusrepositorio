b889fc45144ee6d9613ae4a29f3dda5d24f9c8bb
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2025-01-26 13:23:45 +0200

    unicafe step5

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const StatisticLine = (props) => {
+  return (
+    <div>
+      <p> {props.text} {props.value}</p>
+    </div>
+  )
+}
+
 const Statistics = (props) => {
   if (props.total == 0){
     return (
@@ -10,12 +18,12 @@ const Statistics = (props) => {
   }
   return (
     <div>
-      <p> good {props.good}</p>
-      <p> neutral {props.neutral}</p>
-      <p> bad {props.bad}</p>
-      <p> all {props.total}</p>
-      <p> average {props.average}</p>
-      <p> positive {props.positive} %</p>
+      <StatisticLine text="good" value={props.good}/>
+      <StatisticLine text="neutral" value={props.neutral}/>
+      <StatisticLine text="bad" value={props.bad}/>
+      <StatisticLine text="all" value={props.total}/>
+      <StatisticLine text="average" value={props.average}/>
+      <StatisticLine text="positive" value={`${props.positive} %`}/>
     </div>
   )
 }
