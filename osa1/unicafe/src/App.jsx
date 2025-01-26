85e3d0fd8ace3c91b2b53f9f483bba83bea42425
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2025-01-26 12:05:57 +0200

    unicafe step2

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -6,6 +6,9 @@ const Statistics = (props) => {
       <p> good {props.good}</p>
       <p> neutral {props.neutral}</p>
       <p> bad {props.bad}</p>
+      <p> all {props.total}</p>
+      <p> average {props.average}</p>
+      <p> positive {props.positive} %</p>
     </div>
   )
 }
@@ -22,17 +25,35 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGoodClick = () => {
-    setGood(good + 1)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    const updatedTotal = updatedGood + neutral + bad
+    setTotal(updatedTotal)
+    setAverage((updatedGood + (bad * (-1)))/updatedTotal)
+    setPositive((updatedGood/updatedTotal)*100)
   }
 
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    const updatedTotal = updatedNeutral + good + bad
+    setTotal(updatedTotal)
+    setAverage((good + (bad * (-1)))/updatedTotal)
+    setPositive((good/updatedTotal)*100)
   }
 
   const handleBadClick = () => {
-    setBad(bad + 1)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    const updatedTotal = updatedBad + good + neutral
+    setTotal(updatedTotal)
+    setAverage((good +(updatedBad * (-1)))/updatedTotal)
+    setPositive((good/updatedTotal)*100)
   }
 
   return (
@@ -51,7 +72,7 @@ const App = () => {
         text='bad'
       />
       <h2>Statistics</h2>
-      <Statistics good={good} neutral={neutral} bad={bad}/>          
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} positive={positive}/>
     </div>
   )
 }
