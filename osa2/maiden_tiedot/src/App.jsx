3ae5c3838022e7ddc4b668812701ae3abf5ede91
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2025-08-12 16:41:43 +0300

    2.18* maiden tiedot, step1

---
+++ osa2/maiden_tiedot/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Countries = (props) => {
+
+  const countriesToShow =  props.countries.filter((country) => country.name.common.toLowerCase().includes(props.filterValue.toLowerCase()))
+  if (countriesToShow.length>10) {
+    return (
+      <p>Too many matches, specify another filter</p>
+    )
+  }
+  if (countriesToShow.length===1){
+    return (
+      <div>
+        {countriesToShow.map(country =>
+        <div key={country.name.common}>
+        <h1> {country.name.common}</h1>
+        <p>Capital: {country.capital}</p>
+        <p>Area: {country.area}</p>
+        <h2>Languages</h2>
+        {Object.entries(country.languages).map(([key, value]) =>
+          <li key={key}>
+            {value}
+          </li>
+        )}
+        <p><img src={country.flags.png}></img></p>
+        </div>
+        )}
+      </div>
+    )
+  } else {
+    return (
+      <div>
+      {countriesToShow.map(country =>
+        <div key={country.name.common}>
+        <p> {country.name.common}</p>
+        </div>
+        )}
+      </div>
+    )}
+}
+
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filterValue, setFilterValue] = useState('')
+  const [message, setMessage] = useState(null)
+
+  const handleFilterChange = (event) => {
+    setFilterValue(event.target.value)
+  }
+
+  useEffect(() => {
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+        .then(response => {
+          setCountries(response.data)
+        })
+  }, [])
+
+  return (
+    <div>
+        find countries <input value={filterValue} onChange={handleFilterChange}/>
+        <Countries countries={countries} filterValue={filterValue} setMessage={setMessage} message={message}></Countries>
+    </div>
+  )
+}
+
+export default App
