620196800efa24bc55ee96137cf3792e3eb65711
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2025-08-19 15:38:39 +0300

    3.9 puhelinluettelon backend step9

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -9,6 +10,7 @@ morgan.token('content', (req) => {
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
+app.use(cors())
 
 let persons = [
     {
