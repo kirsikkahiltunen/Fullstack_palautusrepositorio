be1245a331b0b320455d0643479e43df497c4a2d
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2025-08-28 12:50:08 +0300

    4.8: blogilistan testit, step 1

---
+++ osa4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,51 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const blogList = [
+
+    {
+        "title": "Testiblogi",
+        "author": "Maija Meikäläinen",
+        "url": "Testiblogi.fi",
+        "likes": 6
+      },
+      {
+        "title": "Arjen reseptit",
+        "author": "Laura Korhonen",
+        "url": "arjenreseptit.com",
+        "likes": 10
+      },
+      {
+        "title": "Koodarin kulma",
+        "author": "Jari Salmi",
+        "url": "koodarinkulma.dev",
+        "likes": 15
+      }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(blogList[0])
+    await blogObject.save()
+    blogObject = new Blog(blogList[1])
+    await blogObject.save()
+    blogObject = new Blog(blogList[2])
+    await blogObject.save()
+})
+
+test('correct number of blogs are returned as json', async () => {
+    const response = await api
+        .get('/api/blogs')
+        .expect('Content-Type', /application\/json/)
+    assert.strictEqual(response.body.length, 3)
+})
+
+after (async () => {
+    await mongoose.connection.close()
+})
