380173e9ce04e2ae06bbc5a8e0d9d6de41ccfae4
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2025-09-02 14:23:16 +0300

    4.17: blogilistan laajennus, step5

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,28 +1,33 @@
 const blogsRouter = require('express').Router()
-const { request } = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
     response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  if (body.title === undefined || body.url === undefined){
-    response.status(400).end()
-  } else {
+  const user = await User.findById(body.userId)
+  if (!user){
+    return response.status(400).json({ error: 'user missing'})
+  }
+
   const newBlog = {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: user.id
   }
   const blog = new Blog(newBlog)
 
   const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
-}
+  user.blogs = user.blogs.concat(savedBlog.id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
--- osa4/blogilista/models/blog.js
+++ osa4/blogilista/models/blog.js
@@ -1,16 +1,27 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-  title: String,
+  title: {
+      type: String,
+      required: true
+  },
   author: String,
-  url: String,
+  url: {
+    type: String,
+    required: true
+  },
   likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 blogSchema.set('toJSON', {
   transform: (document, blogObject) => {
     blogObject.id = blogObject._id.toString()
     delete blogObject._id
+    delete blogObject.__v
   }
 })
 
