2abdf14703d30ce28f8e8f9f47dd59cff0f7537a
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2025-09-02 14:59:39 +0300

    4.18: blogilistan laajennus, step6

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -15,6 +16,7 @@ mongoose.connect(config.mongoUrl)
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
