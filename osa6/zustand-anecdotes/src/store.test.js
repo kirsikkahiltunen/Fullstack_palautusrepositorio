9124e85ef20115b2a17617e967b90f3a57762f52
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2026-05-14 10:44:37 +0300

    6.13 anekdootit, step12

--- osa6/zustand-anecdotes/src/store.test.js
+++ osa6/zustand-anecdotes/src/store.test.js
@@ -23,7 +23,7 @@ describe('useAnecdoteActions', () => {
         const mockAnecdotes = [{
             "content": "Test anecdote",
             "votes": 3,
-            "id": "47145"
+            "id": "1"
             }
         ]
         anecdoteService.getAll.mockResolvedValue(mockAnecdotes)
@@ -37,5 +37,35 @@ describe('useAnecdoteActions', () => {
         const { result: anecdoteResult } = renderHook(() => useAnecdotes())
         expect(anecdoteResult.current).toEqual((mockAnecdotes))
     })
-    it 
+    it('store returns anecdotes sorted by votes', async() => {
+         const mockAnecdotes = [{
+            "content": "Test anecdote",
+            "votes": 3,
+            "id": "1"
+            },
+            {
+            "content": "Another anecdote",
+            "votes": 2,
+            "id": "2"
+            },
+            {
+            "content": "New anecdote",
+            "votes": 7,
+            "id": "3"
+            }
+        ]
+
+        anecdoteService.getAll.mockResolvedValue(mockAnecdotes)
+
+        const { result } = renderHook(() => useAnecdoteActions())
+
+        await act(async () => {
+            await result.current.initialize()
+        })
+
+        const { result: anecdoteResult } = renderHook(() => useAnecdotes())
+        expect(anecdoteResult.current[0]).toEqual((mockAnecdotes[2]))
+        expect(anecdoteResult.current[1]).toEqual((mockAnecdotes[0]))
+        expect(anecdoteResult.current[2]).toEqual((mockAnecdotes[1]))
+    })
 })
