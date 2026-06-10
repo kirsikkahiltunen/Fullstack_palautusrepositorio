0f3ae7b3cf429512e6255b3111b0aebb6e7cb58b
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2026-06-10 11:48:53 +0300

    7.16: Users view

--- osa7/BlogList/client/src/App.jsx
+++ osa7/BlogList/client/src/App.jsx
@@ -1,71 +1,86 @@
-import { useState, useEffect } from "react";
-import { Routes, Route, Link, useNavigate, useMatch } from "react-router-dom";
-import { Container, AppBar, Toolbar, Button, Typography } from "@mui/material";
-import blogService from "./services/blogs";
-import loginService from "./services/login";
-import ErrorNotification from "./components/ErrorNotification";
-import SuccessNotification from "./components/SuccessNotification";
-import CreateNewBlogForm from "./components/CreateNewBlogForm";
-import BlogList from "./components/BlogList";
-import LoginForm from "./components/LoginForm";
-import Blog from "./components/Blog";
-import ErrorBoundary from "./components/ErrorBoundary";
-import NotFound from "./components/NotFound";
+import { useState, useEffect } from "react"
+import { Routes, Route, Link, useNavigate, useMatch } from "react-router-dom"
+import { Container, AppBar, Toolbar, Button, Typography } from "@mui/material"
+import blogService from "./services/blogs"
+import loginService from "./services/login"
+import userService from "./services/users"
+import ErrorNotification from "./components/ErrorNotification"
+import SuccessNotification from "./components/SuccessNotification"
+import CreateNewBlogForm from "./components/CreateNewBlogForm"
+import BlogList from "./components/BlogList"
+import LoginForm from "./components/LoginForm"
+import Blog from "./components/Blog"
+import UserList from "./components/UserList"
+import ErrorBoundary from "./components/ErrorBoundary"
+import NotFound from "./components/NotFound"
 
 const App = () => {
-  const [blogs, setBlogs] = useState([]);
-  const [user, setUser] = useState(null);
-  const [successMessage, setSuccessMessage] = useState(null);
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [blogs, setBlogs] = useState([])
+  const [users, setUsers] = useState([])
+  const [user, setUser] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
-  const navigate = useNavigate();
-  const match = useMatch("/blogs/:id");
-  const blog = match ? blogs.find((blog) => blog.id === match.params.id) : null;
+  const navigate = useNavigate()
+  const match = useMatch("/blogs/:id")
+  const blog = match ? blogs.find((blog) => blog.id === match.params.id) : null
 
   useEffect(() => {
     blogService.getAll().then((blogs) =>
       setBlogs(
         blogs
           .sort(function (a, b) {
-            return a.likes - b.likes;
+            return a.likes - b.likes
           })
           .reverse(),
       ),
-    );
-  }, []);
+    )
+  }, [])
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem("loggedUser");
+    userService.getAll().then((users) =>
+      setUsers(
+        users
+          .sort(function (a, b) {
+            return a.blogs.length - b.blogs.length
+          })
+          .reverse(),
+      ),
+    )
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedUser")
     if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON);
-      setUser(user);
-      blogService.setToken(user.token);
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
     }
-  }, []);
+  }, [])
 
   const handleLogin = async (userObject) => {
     try {
       const user = await loginService.login({
         username: userObject.username,
         password: userObject.password,
-      });
-      window.localStorage.setItem("loggedUser", JSON.stringify(user));
-      blogService.setToken(user.token);
-      setUser(user);
-      navigate("/");
+      })
+      window.localStorage.setItem("loggedUser", JSON.stringify(user))
+      blogService.setToken(user.token)
+      setUser(user)
+      navigate("/")
     } catch {
-      setErrorMessage("Incorrect username or password");
+      setErrorMessage("Incorrect username or password")
       setTimeout(() => {
-        setErrorMessage(null);
-      }, 5000);
+        setErrorMessage(null)
+      }, 5000)
     }
-  };
+  }
 
   const handleLogout = () => {
-    window.localStorage.removeItem("loggedUser");
-    setUser(null);
-    navigate("/");
-  };
+    window.localStorage.removeItem("loggedUser")
+    setUser(null)
+    navigate("/")
+  }
 
   const addNewBlog = (blogObject) => {
     blogService.create(blogObject).then(() => {
@@ -73,20 +88,20 @@ const App = () => {
         setBlogs(
           blogs
             .sort(function (a, b) {
-              return a.likes - b.likes;
+              return a.likes - b.likes
             })
             .reverse(),
         ),
-      );
+      )
       setSuccessMessage(
         `A new blog ${blogObject.title} by ${blogObject.author} added`,
-      );
-      navigate("/");
+      )
+      navigate("/")
       setTimeout(() => {
-        setSuccessMessage(null);
-      }, 5000);
-    });
-  };
+        setSuccessMessage(null)
+      }, 5000)
+    })
+  }
 
   const addLikes = (blogObject) => {
     blogService.addLike(blogObject.id, blogObject).then(() => {
@@ -94,13 +109,13 @@ const App = () => {
         setBlogs(
           blogs
             .sort(function (a, b) {
-              return a.likes - b.likes;
+              return a.likes - b.likes
             })
             .reverse(),
         ),
-      );
-    });
-  };
+      )
+    })
+  }
 
   const deleteBlogs = (blogObject) => {
     blogService.deleteBlog(blogObject.id).then(() => {
@@ -108,16 +123,16 @@ const App = () => {
         setBlogs(
           blogs
             .sort(function (a, b) {
-              return a.likes - b.likes;
+              return a.likes - b.likes
             })
             .reverse(),
         ),
-      );
-      navigate("/");
-    });
-  };
+      )
+      navigate("/")
+    })
+  }
 
-  const style = { "&:hover": { bgcolor: "rgba(255, 255, 255, 0.5)" } };
+  const style = { "&:hover": { bgcolor: "rgba(255, 255, 255, 0.5)" } }
 
   return (
     <Container>
@@ -130,6 +145,9 @@ const App = () => {
               </Typography>
               <Button color="inherit" component={Link} to="/" sx={style}>
                 blogs
+              </Button>
+              <Button color="inherit" component={Link} to="/users" sx={style}>
+                users
               </Button>
               {user && (
                 <Button
@@ -191,6 +209,14 @@ const App = () => {
             }
           />
           <Route
+            path="/users"
+            element={
+              <ErrorBoundary>
+                <UserList users={users} />
+              </ErrorBoundary>
+            }
+          />
+          <Route
             path="/login"
             element={
               <ErrorBoundary>
@@ -205,7 +231,7 @@ const App = () => {
         </Routes>
       </div>
     </Container>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ osa7/BlogList/client/src/components/UserList.jsx
@@ -0,0 +1,39 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material"
+
+const UserList = ({ users }) => {
+  return (
+    <div>
+      <h2>Users</h2>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Username</TableCell>
+              <TableCell>Number of blogs created</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.username}</TableCell>
+                <TableCell>{user.blogs.length}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  )
+}
+
+export default UserList
